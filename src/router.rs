a2f6cc79dc99971b6942eac660fa3f3710910dcc
Dduo <lploh2016@gmail.com>
2019-01-29 18:49:28 -0800

    0

--- src/router.rs
+++ src/router.rs
@@ -3,9 +3,10 @@
 #![cfg_attr(feature = "cargo-clippy", allow(needless_pass_by_value))]
 
 use actix_web::{
-    App, fs, middleware, pred, actix::Addr,
+    App, fs, pred, actix::Addr,
     HttpRequest, HttpResponse, Result,
     http::{self, header, Method, StatusCode},
+    middleware::{self, cors::Cors},
 };
 use db::dba::{ Dba, init };
 use api::{index::hello};
@@ -20,29 +21,22 @@ pub fn app_with_state() -> App<AppState> {
     .middleware(middleware::Logger::default())        
     // register simple route, handle all methods
     .resource("/", |r| r.f(index))
-    .resource("/{name}", |r| r.method(http::Method::GET).with(hello))
+    .configure( |app| Cors::for_app(app)
+        .max_age(3600)
+        // register routes
+        .resource("/api/home", |r| {})
+        .register()
+    )
     // static files
     .handler("/static", fs::StaticFiles::new("static").unwrap())
     // redirect
-    .resource("/", |r| r.method(Method::GET).f(|req| {
-        println!("{:?}", req);
-        HttpResponse::Found()
-            .header(header::LOCATION, "static/index.html")
-            .finish()
-    }))
+    .resource("/", |r| { /* todo: redirect */ })
     // default
-    .default_resource(|r| {
-        // 404 for GET request
-        r.method(Method::GET).f(not_found);
-
-        // all requests that are not `GET`
-        r.route().filter(pred::Not(pred::Get()))
-         .f(|req| HttpResponse::MethodNotAllowed());
-    })
+    .default_resource(|r| { /* todo: default, for 404, etc. */ })
 }
 
-// some simple handler, index, 404...
-// index handler
+
+// index handler, just for try
 fn index(req: &HttpRequest<AppState>) -> Result<HttpResponse> {
     println!("{:?}", req);
 
@@ -50,9 +44,4 @@ fn index(req: &HttpRequest<AppState>) -> Result<HttpResponse> {
     Ok(HttpResponse::build(StatusCode::OK)
         .content_type("text/html; charset=utf-8")
         .body(include_str!("../static/index.html")))
-}
-
-// 404 handler
-fn not_found(req: &HttpRequest<AppState>) -> Result<fs::NamedFile> {
-    Ok(fs::NamedFile::open("static/404.html")?.set_status_code(StatusCode::NOT_FOUND))
 }
